1dd1c5aee1ea1ca4d4993a89568b044141517df1
Resfort <ssgas007@gmail.com>
2026-09-02 20:37:24 +0300

    Update Bank_Account.py
    def deposit и def withdraw

--- Bank_Account.py
+++ Bank_Account.py
@@ -1,5 +1,15 @@
 
-class user:
+class User:
     def __init__(self, name, balance):
         self.name = name
         self.balance = balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance = self.balance + amount
+
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.balance:
+            self.balance = self.balance - amount
+        else:
+            print("Insufficient funds or invalid amount.")
